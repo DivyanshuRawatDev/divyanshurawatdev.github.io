001223d49535cc29d985bd61a7ed5bd57fdbad0d
Divyanshu766 <divyanshu.rawat.dev@gmail.com>
2024-03-15 00:49:17 +0530

    fix bugs

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const resumeBtn = document.querySelector(".resume-btn");
 
 resumeBtn.addEventListener("click", () => {
-  // Construct the direct download link from the Google Drive link
-  const fileID = "16zgiucW79OvmKctW9H0DZ0TqY7JSgZyA"; // Extracted from the original link
+  const fileID = "16zgiucW79OvmKctW9H0DZ0TqY7JSgZyA";
   const directDownloadLink = `https://drive.google.com/uc?export=download&id=${fileID}`;
-  //   const directLink = `https://drive.google.com/file/d/${fileID}/preview`;
 
   const downloadLink = document.createElement("a");
 
@@ -15,5 +13,4 @@ resumeBtn.addEventListener("click", () => {
   document.body.appendChild(downloadLink);
 
   downloadLink.click();
-  //   window.location.href = directLink;
 });
